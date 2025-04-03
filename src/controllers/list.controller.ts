e5b5655932bad100e3336b34b92fe046d02e7f76
marutina98 <martinamineo98@gmail.com>
2025-04-03 21:39:12 +0200

    Feat: working on CList

--- src/controllers/list.controller.ts
+++ src/controllers/list.controller.ts
@@ -1,6 +1,109 @@
 import prisma from './../prisma';
+import { verify } from 'jsonwebtoken';
+
+// Types and Interfaces
+
+import { Request, Response } from 'express';
+import IError from '../interfaces/error.interface';
+import IDecodedToken from '../interfaces/decoded-token.interface';
 
 class CList {
+
+  public async getList(req: Request, res: Response, next: Function) {
+
+    try {
+
+      // If a list is private show only
+      // if the user is logged and is the author
+
+      const id = req.params.id;
+
+      // If the id is not valid throw error
+
+      if (!id) {
+        const error = (new Error('Valid Id not found.')) as IError;
+        error.status = 404;
+        throw error;
+      }
+
+      // Get List or throw error
+
+      const list = await prisma.list.findUniqueOrThrow({
+        where: {
+          id
+        },
+        include: {
+          priority: true,
+          groups: {
+            include: {
+              items: true
+            }
+          }
+        }
+      });
+
+      // If the list is public return
+      // otherwise check if the authenticated user is
+      // the author
+
+      if (!list.private) {
+        return res.status(200).json(list);
+      }
+
+      // Check if user is authenticated
+
+      const authorizationHeader = req.headers.authorization ?? null;
+            
+      if (!authorizationHeader) {
+        const error = (new Error('User is not authenticated')) as IError;
+        error.status = 403;
+        throw error;
+      }
+
+      // Get Token from Authorization Header
+
+      const token = authorizationHeader?.split(' ')[1];
+      const decodedToken = verify(token, 'JWT_SECRET') as IDecodedToken;
+      
+      if (!decodedToken) {
+        const error = (new Error('Token is not present.')) as IError;
+        error.status = 403;
+        throw error;
+      }
+
+      // Get User via Email decoded from Token
+      // Find user or throw error
+
+      const email = decodedToken.email;
+
+      if (!email) {
+        const error = new Error('Email could not be found in token.') as IError;
+        error.status = 500;
+        throw error;
+      }
+
+      // Find authenticated User
+      // check that authenticated user is the author
+
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          email
+        }
+      });
+
+      if (user.id === list.userId) {
+        res.status(200).json(list);
+      }
+
+      const error = new Error(`Authenticated User is not the Author of List with id ${id}`) as IError;
+      error.status = 401;
+      throw error;
+
+    } catch (error: unknown) {
+      return next(error);
+    }
+
+  }
 
 }
 
--- src/routes/list.routes.ts
+++ src/routes/list.routes.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 import CList from '../controllers/list.controller';
 
+// Types and Interfaces
+
+import { Request, Response } from 'express';
+
 const router = express.Router();
+
+router.get('/:id', (req: Request, res: Response, next: Function) => CList.getList(req, res, next));
 
 export default router;
