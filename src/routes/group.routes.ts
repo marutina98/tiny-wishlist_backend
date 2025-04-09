0c7c2bf0131890e4326323d685c55792dda477b8
marutina98 <martinamineo98@gmail.com>
2025-04-09 16:02:07 +0200

    Feat: workin in RGroup

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -3,10 +3,16 @@ import CGroup from '../controllers/group.controller';
 
 import { Request, Response, NextFunction } from 'express';
 
+// Middlewares
+
+import MOwnerGroup from '../middlewares/owner-group.middleware';
+import MIsAuthenticated from '../middlewares/auth.middleware';
+
 const router = express.Router();
 
-router.post('/', (req: Request, res: Response, next: NextFunction) => CGroup.createGroup(req, res, next));
-router.put('/:id/archival', (req: Request, res: Response, next: NextFunction) => CGroup.changeArchivalStatus(req, res, next));
-router.delete('/:id', (req: Request, res: Response, next: NextFunction) => CGroup.deleteGroup(req, res, next));
+router.post('/', MIsAuthenticated, (req: Request, res: Response, next: NextFunction) => CGroup.createGroup(req, res, next));
+router.put('/:id', MOwnerGroup, (req: Request, res: Response, next: NextFunction) => CGroup.putGroup(req, res, next));
+router.put('/:id/archival', MOwnerGroup, (req: Request, res: Response, next: NextFunction) => CGroup.changeArchivalStatus(req, res, next));
+router.delete('/:id', MOwnerGroup, (req: Request, res: Response, next: NextFunction) => CGroup.deleteGroup(req, res, next));
 
 export default router;
