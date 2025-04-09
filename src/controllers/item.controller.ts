ae52b70a50c6295cf6fd8ddf5af64fdde94d976f
marutina98 <martinamineo98@gmail.com>
2025-04-09 19:07:51 +0200

    Feat: working in CItem and RItem

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -9,21 +9,80 @@ class CItem {
 
   public async createItem(req: Request, res: Response, next: NextFunction) {
 
+    try {
+
+    } catch (error: unknown) {
+      return next(error);
+    }
+
   }
 
   public async putItem(req: Request, res: Response, next: NextFunction) {
+
+    try {
+
+    } catch (error: unknown) {
+      return next(error);
+    }
 
   }
 
   public async changeArchivalStatus(req: Request, res: Response, next: NextFunction) {
 
+    try {
+
+    } catch (error: unknown) {
+      return next(error);
+    }
+
   }
 
   public async changeReservedStatus(req: Request, res: Response, next: NextFunction) {
 
+    try {
+
+    } catch (error: unknown) {
+      return next(error);
+    }
+
   }
 
   public async deleteItem(req: Request, res: Response, next: NextFunction) {
+
+    try {
+
+      const id = req.params.id;
+      
+      if (!id) {
+        const error = (new Error('Valid Id not found.')) as IError;
+        error.status = 404;
+        throw error;
+      }
+
+      // Check if item exists
+      // if not found, throw error
+
+      await prisma.item.findUniqueOrThrow({
+        where: {
+          id
+        }
+      });
+
+      // Delete item
+
+      await prisma.item.delete({
+        where: {
+          id
+        }
+      });
+
+      res.status(200).json({
+        message: `Item with id ${id} was successfully deleted`
+      });
+
+    } catch (error: unknown) {
+      return next(error);
+    }
 
   }
 
--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import CItem from '../controllers/item.controller';
+import MIsAuthenticated from '../middlewares/auth.middleware';
+
+import { NextFunction, Request, Response } from 'express';
 
 const router = express.Router();
+
+router.put('/', MIsAuthenticated, (req: Request, res: Response, next: NextFunction) => CItem.createItem(req, res, next));
 
 export default router;
