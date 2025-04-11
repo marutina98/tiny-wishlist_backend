0ee6a3426d2bbe79cf568471c387236fd7fcc24c
marutina98 <martinamineo98@gmail.com>
2025-04-11 10:21:06 +0200

    Feat: working in CItem still

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -177,6 +177,35 @@ class CItem {
 
     try {
 
+      // Get the item or throw error
+      
+      const id = req.params.id;
+
+      if (!id) {
+        const error = (new Error('Valid Id not found.')) as IError;
+        error.status = 404;
+        throw error;
+      }
+
+      const ogItem = await prisma.item.findUniqueOrThrow({
+        where: {
+          id
+        }
+      });
+
+      // Toggle the value of ogItem.reserved and return it
+
+      const item = await prisma.item.update({
+        where: {
+          id
+        },
+        data: {
+          reserved: !ogItem.reserved
+        }
+      });
+
+      res.status(200).json(item);
+
     } catch (error: unknown) {
       return next(error);
     }
