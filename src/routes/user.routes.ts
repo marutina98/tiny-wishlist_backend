0b8b45790a513b1fde7d824cfbbde30887e919f1
marutina98 <martinamineo98@gmail.com>
2025-04-12 21:16:14 +0200

    Fix: added missing route putUser

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -12,6 +12,7 @@ import MIsAuthenticated from '../middlewares/auth.middleware';
 const router = express.Router();
 
 router.get('/auth', MIsAuthenticated, (req: Request, res: Response, next: NextFunction) => CUser.getAuthenticatedUser(req, res, next));
+router.put('/', MIsAuthenticated, (req: Request, res: Response, next: NextFunction) => CUser.putUser(req, res, next));
 router.delete('/', MIsAuthenticated, (req: Request, res: Response, next: NextFunction) => CUser.deleteUser(req, res, next));
 
 export default router;
