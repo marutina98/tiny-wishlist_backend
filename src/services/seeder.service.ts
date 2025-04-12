fd250debe842e15e927168a9a2055e256b238eed
marutina98 <martinamineo98@gmail.com>
2025-04-12 19:31:25 +0200

    Feat: working on seeder

--- src/services/seeder.service.ts
+++ src/services/seeder.service.ts
@@ -1,4 +1,7 @@
 import prisma from '../prisma';
+import { faker } from '@faker-js/faker';
+
+import { User, Priority, List, Group, Item } from '@prisma/client';
 
 class SSeeder {
 
@@ -7,6 +10,16 @@ class SSeeder {
     // Seed 'Low', 'Medium' and 'High' priorities
 
     await this.seedPriorities();
+
+    // Seed Users
+
+    const users = await this.seedUsers();
+
+    // Seed Lists for each user
+
+    for (let user of users) {
+      const lists = await this.seedLists(user);
+    }
 
   }
 
@@ -34,19 +47,53 @@ class SSeeder {
 
   }
 
-  public async seedUsers() {
+  public async seedUsers(num: number = 5) {
+
+    const users: User[] = [];
+
+    // Generate *num* of users
+
+    for (let i = 0; i <= num; i++) {
+
+      const options = {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+      }
+
+      const data = {
+        email: faker.internet.email(options),
+        username: faker.internet.username(options),
+        password: 'password'
+      }
+
+      const user = await prisma.user.create({ data });
+      users.push(user);
+
+    }
+
+    return users;
 
   }
 
-  public async seedLists() {
+  public async seedLists(user: User, num: number = 5) {
+
+    const lists: List[] = [];
+
+    for (let i = 0; i <= num; i++) {
+
+
+
+    }
+    
+    return lists;
 
   }
 
-  public async seedGroups() {
+  public async seedGroups(list: List) {
 
   }
 
-  public async seedItems() {
+  public async seedItems(group: Group) {
 
   }
 
