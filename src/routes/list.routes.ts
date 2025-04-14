f1fbfaa5918da777951ebc9e20e22226c2f91fd2
marutina98 <martinamineo98@gmail.com>
2025-04-14 15:09:44 +0200

    Fix: added missing route in RList

--- src/routes/list.routes.ts
+++ src/routes/list.routes.ts
@@ -18,6 +18,7 @@ const router = express.Router();
 
 router.post('/', MIsAuthenticated, (req: Request, res: Response, next: NextFunction) => CList.createList(req, res, next));
 router.get('/:id', MList, (req: Request, res: Response, next: NextFunction) => CList.getList(req, res, next));
+router.put('/:id', MIsAuthenticated, MOwnerList, (req: Request, res: Response, next: NextFunction) => CList.putList(req, res, next));
 router.put('/:id/visibility', MIsAuthenticated, MOwnerList, (req: Request, res: Response, next: NextFunction) => CList.changeVisibilityStatus(req, res, next));
 router.put('/:id/archival', MIsAuthenticated, MOwnerList, (req: Request, res: Response, next: NextFunction) => CList.changeArchivalStatus(req, res, next));
 router.put('/:id/priority', MIsAuthenticated, MOwnerList, (req: Request, res: Response, next: NextFunction) => CList.changePriority(req, res, next));
