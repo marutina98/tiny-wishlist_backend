e24efecc7c01023eb17ca36143ab859a3808cafe
marutina98 <martinamineo98@gmail.com>
2025-04-19 15:04:29 +0200

    Fix: fixed backend error in MList

--- src/middlewares/list.middleware.ts
+++ src/middlewares/list.middleware.ts
@@ -86,6 +86,10 @@ export default async function MList(req: IRequestUser, res: Response, next: Next
 
       }
 
+      const error = new Error('List could not be accessed.') as IError;
+      error.status = 401;
+      throw error;
+
     }
 
   } catch (error: unknown) {
