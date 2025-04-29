c5503ba79ccfd21b2d09f089e684c4fd0d4f933c
marutina98 <martinamineo98@gmail.com>
2025-04-29 17:35:04 +0200

    Fix: removed unneeded routes

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -10,8 +10,6 @@ const router = express.Router();
 
 router.post('/', MIsAuthenticated, (req: Request, res: Response, next: NextFunction) => CItem.createItem(req, res, next));
 router.put('/:id', MIsAuthenticated, MOwnerItem, (req: Request, res: Response, next: NextFunction) => CItem.putItem(req, res, next));
-router.put('/:id/archival', MIsAuthenticated, MOwnerItem, (req: Request, res: Response, next: NextFunction) => CItem.changeArchivalStatus(req, res, next));
-router.put('/:id/reserved', MIsAuthenticated, MOwnerItem, (req: Request, res: Response, next: NextFunction) => CItem.changeReservedStatus(req, res, next));
 router.delete('/:id', MIsAuthenticated, MOwnerItem, (req: Request, res: Response, next: NextFunction) => CItem.deleteItem(req, res, next));
 
 export default router;
